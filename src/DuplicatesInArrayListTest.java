fbd472232149b4504b01cb404f2da96a7e8952a7
burtonwynne <burton.wynne.vectormax@gmail.com>
2016-06-07 15:21:08 -0400

    Removed has duplicates method and created removeDuplicates method.

--- src/DuplicatesInArrayListTest.java
+++ src/DuplicatesInArrayListTest.java
@@ -4,53 +4,60 @@ import java.util.Random;
 
 public class DuplicatesInArrayListTest {
 	
-	
-	public <E> boolean hasDuplicates(ArrayList<E> list){
-		boolean hasDuplicates = false;
-		int length = list.size();
+	public <E> void removeDuplicates(ArrayList<E> list){
+		System.out.println("Before: " + list);
 		E currItem;  
-		for(int index1 = 0; index1 < (length -1); index1++){
+		for(int index1 = 0; index1 < (list.size() -1); index1++){
 			currItem = list.get(index1);
-			for(int index2 = (index1 + 1); index2 < length; index2++){
+			for(int index2 = (index1 + 1); index2 < list.size(); index2++){
 				if(currItem.equals(list.get(index2))){
-					hasDuplicates = true;
-					return hasDuplicates;
+					list.remove(index2);
+					index2--;
+					
 				}
 			}
 		}
-		return hasDuplicates;
+		System.out.println("After: " + list);
 	}
 	
 	public static void main(String[] args) {
 		DuplicatesInArrayListTest dialt = new DuplicatesInArrayListTest();
 		ArrayList<Integer> testList = new ArrayList<>();
+		
+		
 		testList.addAll(Arrays.asList(1, 2, 3, 4, 5));
-		System.out.println(testList + " has Duplicates: " + dialt.hasDuplicates(testList));
+		dialt.removeDuplicates(testList);
 		testList.clear();
 		
 		testList.addAll(Arrays.asList(1, 2, 3, 4, 5, 1));
-		System.out.println(testList + " has Duplicates: " + dialt.hasDuplicates(testList));
+		dialt.removeDuplicates(testList);
 		testList.clear();
 		
 		testList.addAll(Arrays.asList(1, 2, 8, 4, 5));
-		System.out.println(testList + " has Duplicates: " + dialt.hasDuplicates(testList));
+		dialt.removeDuplicates(testList);
 		testList.clear();
 		
 		testList.addAll(Arrays.asList(1, 2, 5, 4, 5));
-		System.out.println(testList + " has Duplicates: " + dialt.hasDuplicates(testList));
+		dialt.removeDuplicates(testList);
 		testList.clear();
 		
 		testList.addAll(Arrays.asList(100, 2000, 89, 43, 51));
-		System.out.println(testList + " has Duplicates: " + dialt.hasDuplicates(testList));
+		dialt.removeDuplicates(testList);
 		testList.clear();
 		
 		Random rdm = new Random();
 		
-		for(int counter = 0; counter < 25; counter++){
-			testList.add(rdm.nextInt(20) + 1);
+		for(int counter = 0; counter < 30; counter++){
+			testList.add(rdm.nextInt(5) + 1);
 		}
-		System.out.println(testList + " has Duplicates: " + dialt.hasDuplicates(testList));
+		
+		dialt.removeDuplicates(testList);
+		testList.clear();
+		
+		for(int counter = 0; counter < 25; counter++){
+			testList.add(4);
+		}
+		dialt.removeDuplicates(testList);
 		testList.clear();
 	}
-
 }
